62e8329d51c66d8d131dbe39722a215ff7a039de
hoonyong <wjaj12@naver.com>
2022-02-11 14:36:28 +0900

    spring 초기 빌드

---
+++ project4_1/src/main/java/com/example/project4_1/Project41Application.java
@@ -0,0 +1,13 @@
+package com.example.project4_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project41Application.class, args);
+	}
+
+}
