806c90d09af3efaaa36ea53ff06e3b40a3901162
gnsdyd12 <wjaj12@naver.com>
2022-05-15 17:03:25 +0900

    마이페이지 url 연결

---
+++ project4_1/src/main/java/com/example/project4_1/mypage/myPageController.java
@@ -0,0 +1,31 @@
+package com.example.project4_1.mypage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/mypage")
+public class myPageController {
+    @GetMapping("/mainpage")
+    public String mainpage(){
+        return "mypage/main_page";
+    }
+
+    @GetMapping("/favoritePost")
+    public String favoritePost(){
+        return "mypage/favorite_post";
+    }
+
+    @GetMapping("/myInfo")
+    public String myInfo(){
+        return "mypage/my_info";
+    }
+
+    @GetMapping("/temporaryPost")
+    public String temporaryPost(){
+        return "mypage/temporary_post";
+    }
+}
